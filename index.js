559276c252613f0f789be3f679b0cd65d54dac19
Glenn Robert Strandos <strandos.glenn@gmail.com>
2020-01-25 00:20:28 +0100

    Change a value in cors policy

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'https://sodnarts.com');
-    res.header('Access-Control-Allow-Credentials', 'https://sodnarts.com');
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
