cf5e5b2f4aa08690f888dd1bb90ec0cb33ffe56a
Glenn Robert Strandos <strandos.glenn@gmail.com>
2020-01-24 22:05:28 +0100

    Enabling CORS

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ mongoose.connect(keys.mongoURI, options, (err) => {
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(
     cookieSession({
