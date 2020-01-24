765c4245f5f15369f3219d12ea3fe0802465fbcb
Glenn Robert Strandos <strandos.glenn@gmail.com>
2020-01-25 00:09:02 +0100

    allow credentials

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const app = express();
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
@@ -28,6 +29,7 @@ app.use(
     cookieSession({
         //Age: days hours min sec milisec
         maxAge: 30 * 24 * 60 * 60 * 1000,
+        domain: 'sodnarts-api',
         keys: [keys.cookieKey],
     }),
 );
