53cb6d2132d7baa8d1cbd00191f34616a095efc9
Glenn Robert Strandos <strandos.glenn@gmail.com>
2020-01-24 22:35:19 +0100

    Restarting Server

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ if (1 == 0) {
     // Express will serve up production assets like our main.js file, or main.css file!
     app.use(express.static('../client/build'));
 
-    // Express will serve up the index.html file, if it doesn't recognize the route
+    // Express will serve up the index.html file, if it doesn't recognize the route.
     const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
