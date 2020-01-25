d57d379af64fa227ec8dd88da023fa4b89f7c9eb
Glenn Robert Strandos <strandos.glenn@gmail.com>
2020-01-25 13:33:50 +0100

    Fix a mistake in cors policy

--- index.js
+++ index.js
@@ -27,10 +27,9 @@ app.use(function(req, res, next) {
     ];
     if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
         res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     }
-    res.header('Access-Control-Allow-Origin', 'https://sodnarts.com');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
