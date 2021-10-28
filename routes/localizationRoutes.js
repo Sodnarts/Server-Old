ec541454835845ce44d8481705ed9f85155c5923
Glenn Robert Strandos <strandos.glenn@gmail.com>
2021-10-28 13:00:24 +0200

    Update localization logging

--- routes/localizationRoutes.js
+++ routes/localizationRoutes.js
@@ -17,7 +17,7 @@ module.exports = (app) => {
 
     app.post('/api/projects/new', async (req, res) => {
         const { title, description, languages, translations } = req.body;
-        console.log(req.body);
+
         const newTranslations = []
         translations.map(t => {
             newTranslations.push(new Translation({strings: t.strings}));
